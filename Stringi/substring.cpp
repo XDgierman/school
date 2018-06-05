05ae5b80b0658e91c76604bebd20f93a5e452a75
XDgierman <xdgierman@interia.pl>
2018-06-05 10:19:55 +0200

    add files

---
+++ Stringi/substring.cpp
@@ -0,0 +1,25 @@
+#include <iostream>  
+
+using namespace std; 
+ 
+string substring (string a,int p, int k){
+	string w="";
+	for (int i=p;i<=k;i++){
+		w+=a[i];
+	}
+	return w;
+}
+ 
+int main(){ 
+cout<<substring("ABCDEFGH",3,6);
+}
+
+
+
+
+
+
+
+
+
+
