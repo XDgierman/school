4838a0ce037cd748044a8b5ddad9cb471eb9b6a8
XDgierman <34095690+XDgierman@users.noreply.github.com>
2018-10-03 09:08:35 +0200

    database excercise + .gitignore update

---
+++ databases/ćwiczenie 5/file8.sql
@@ -0,0 +1,2 @@
+SELECT SUM(Ilosc*CenaJednostkowa) AS [Całkowity przychód]
+FROM SzczegolySprzedazy;
---
+++ databases/ćwiczenie 5/file9.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) AS [Liczba sprzedaży]
+FROM Sprzedaze
+WHERE DataSprzedazy > #2013-01-01#;
