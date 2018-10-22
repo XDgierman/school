3bd4419aa707fe83a07c87ab9adae294ea861f53
XDgierman <34095690+XDgierman@users.noreply.github.com>
2018-10-22 08:39:03 +0200

    excercises

--- c++/zad5p2.cpp
+++ c++/zad5p2.cpp
@@ -3,20 +3,48 @@
 
 using namespace std;
 
+string reverse(string a){
+  string w="";
+  for (int i=a.length()-1;i>=0;i--){
+	w+=a[i];
+}
+return w;
+}
 
 int main(){
 ifstream file;
 ifstream nowe;
 file.open("slowa.txt");
-nowe.open("nowe.txt")
+nowe.open("nowe.txt");
 string a;
 string t1[25];
 string t2[25];
+int t1i[25], t2i[25];
 for (int i=0;i<25;i++){
-  t[i]=;
-  t[i+1]=
+  nowe>>t1[i];
+  t2[i]=reverse(t1[i]);
+  /*cout<<t1[i];
+  cout.width(20);
+  cout<<t2[i]<<endl;  */
+  t1i[i]=0;
+  t2i[i]=0;
 }
 for (int i=0;i<1000;i++){
 file>>a;
+for (int j=0;j<25;j++){
+  if (a==t1[j]){
+    t1i[j]++;
+  }
+  if (a==t2[j]){
+    t2i[j]++;
+  }
+}
+}
+for (int i=0;i<25;i++){
+  cout<<t1[i];
+  cout.width(10+(12-t1[i].length()));
+  cout<<t1i[i];
+  cout.width(10);
+  cout<<t2i[i]<<endl;
 }
 }
