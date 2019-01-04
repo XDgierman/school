f8c3a7434d8c988e63d2dd8e69bec4540d807da9
XDgierman <34095690+XDgierman@users.noreply.github.com>
2019-01-04 11:43:31 +0100

    php excercise

---
+++ html/javascript/php/data_types.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $txt = "Hello world!";
+    $x = 10.5;
+    $y = 19;
+    $h = 0xAB;
+    $o = 0312;
+    $b = true;
+    $cars = array("Volvo","BMW","Toyota");
+    class Car {
+      function Car() {
+        $this->model = "VW";
+      }
+    }
+    $herbie = new Car();
+    $n = null;
+    var_dump($txt);
+    echo "<br>";
+    var_dump($x);
+    echo "<br>";
+    var_dump($y);
+    echo "<br>";
+    var_dump($h);
+    echo "<br>";
+    var_dump($o);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($cars);
+    echo "<br>";
+    var_dump($n);
+    echo "<br>";
+    var_dump($herbie);
+    ?>
+  </body>
+</html>
