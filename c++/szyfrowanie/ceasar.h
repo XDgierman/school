afca8a123582b4b4e7b902c6326c636e94e775b5
XDgierman <34095690+XDgierman@users.noreply.github.com>
2019-01-15 10:36:23 +0100

    ceasar encryption and decryption library

---
+++ c++/szyfrowanie/ceasar.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+string caesar (string a, string n){
+	int k, in=0, wn;
+	string w="";
+	for (int i=0;i<a.length();i++){
+		if (in==n.length()){
+			in=0;
+			wn=(int)n[in]-64;
+			in++;
+		} else {
+			wn=(int)n[in]-64;
+			in++;
+		}
+		k=(int)a[i]-65;
+		k+=wn;
+		if (k>=26) k%=26;
+		w+=(char)(k+65);
+	}
+	return w;
+}
+string decaesar (string a, string n){
+	int k, in=0, wn;
+	string w="";
+	for (int i=0;i<a.length();i++){
+		if (in==n.length()){
+			in=0;
+			wn=(int)n[in]-64;
+			in++;
+		} else {
+			wn=(int)n[in]-64;
+			in++;
+		}
+		k=(int)a[i]-65;
+		k-=wn;
+		while (k<=0){
+			k+=26;
+		}
+		w+=(char)(k+65);
+	}
+	return w;
+}
