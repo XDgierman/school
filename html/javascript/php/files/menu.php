d3e231f35bbecab3f0fd064e8457b3a771dbaf5d
XDgierman <34095690+XDgierman@users.noreply.github.com>
2019-02-08 14:25:20 +0100

    php file operation functions

---
+++ html/javascript/php/files/menu.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<a href='index.php'>Główna</a>";
+	echo "<a href='podstrona1.php'>Podstrona1</a>";
+	echo "<a href='podstrona2.php'>Podstrona2</a>";
+	echo "<a href='podstrona3.php'>Podstrona3</a>";
+?>
