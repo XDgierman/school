41c422f117ca5c384f06e8faf6b275873860dd31
XDgierman <34095690+XDgierman@users.noreply.github.com>
2019-03-04 10:36:22 +0100

    Create enigma.cpp

---
+++ c++/szyfrowanie/enigma.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+void enigma(){
+char a[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+int bi[26];
+int ibi=0;
+for (int i=0;i<26;i++){
+  bi[i]=30;
+}
+while(ibi != 27){
+  for
+}
+}
+
+int main() {
+  enigma();
+return 0;
+}
