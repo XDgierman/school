b590b2ff2a5fc859da2f5ee22af78e5c6185b2d1
XDgierman <34095690+XDgierman@users.noreply.github.com>
2019-04-29 10:36:49 +0200

    starting java

---
+++ java/project1/src/project1/Program1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+package project1;
+
+public class Program1 {
+	public static void main(String[] args) {
+		Scanner ob=new Scanner(System.in);
+		String X=ob.nextLine();
+		int x=Integer.parseInt(X);
+	}
+}
