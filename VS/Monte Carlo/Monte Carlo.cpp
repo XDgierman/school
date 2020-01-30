8813aeee9d16e2ec416e1d95b4c8911e1cf207e4
XDgierman <34095690+XDgierman@users.noreply.github.com>
2020-01-30 15:21:26 +0100

    monte carlo method

---
+++ VS/Monte Carlo/Monte Carlo.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+/*bool monte(int x, int y, bool carlo = false) {
+    int xy, r = 200;
+    xy = sqrt(x - r) + sqrt(y - r);
+    if (carlo == false and xy < sqrt(r)) {
+        return true;
+    }
+    else if (carlo == true and xy == sqrt(r)) {
+        return true;
+    }
+    else return false;
+}*/
+
+bool monte(int x, int y) {
+    int xy, r = 200;
+    xy = (x - r) * (x - r) + (y - r) * (y - r);
+    cout << xy << endl;
+    if (xy < (r*r)) {
+        return true;
+    }
+    else return false;
+}
+
+int main()
+{
+    ifstream file;
+    file.open("punkty.txt");
+    int count = 0;
+    int x, y;
+    for (int i = 0; i < 100; i++)
+    {
+        file >> x >> y;
+        if (monte(x, y)) {
+            count++;
+        }
+    }
+    cout << count << endl;
+    cin.get();
+    return 0;
+}
